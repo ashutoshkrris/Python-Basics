2c682f184726512e8fe7914720e9af1f8c275654
Ashutosh Krishna <47353498+ashutoshkrris@users.noreply.github.com>
2020-02-08 14:31:46 +0530

    Addition of two numbers

---
+++ P2.py
@@ -0,0 +1,10 @@
+num1 = int(input("Enter first number : "))
+num2 = int(input("Enter second number : "))
+print("Sum : "+str(num1+num2))
+
+
+'''                Output
+            Enter first number : 3
+            Enter second number : 4
+            Sum : 7
+'''
