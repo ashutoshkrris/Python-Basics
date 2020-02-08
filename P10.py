f8dbbd067d1a8cca1e122e267942f3a2d0192b04
Ashutosh Krishna <47353498+ashutoshkrris@users.noreply.github.com>
2020-02-08 15:35:47 +0530

    Check if a number is odd or even

---
+++ P10.py
@@ -0,0 +1,11 @@
+num = int(input("Enter the number : "))
+if(num%2==0):
+    print("Even Number")
+else:
+    print("Odd Number")
+
+
+'''                                 Output
+                            Enter the number : 3
+                            Odd Number
+'''
