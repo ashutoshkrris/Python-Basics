fe4fb231ec9a3686e6ebb354a9066f14bb76973e
Ashutosh Krishna <47353498+ashutoshkrris@users.noreply.github.com>
2020-02-08 15:28:10 +0530

    Check if a number is positive,negative or zero

---
+++ P9.py
@@ -0,0 +1,14 @@
+num = float(input("Enter the number : "))
+if(num>=0):
+    if(num==0):
+        print("Zero")
+    else:
+        print("Positive Number")
+else:
+    print("Negative Number")
+
+
+'''                                     Output
+                                Enter the number : 13
+                                Positive Number
+'''
