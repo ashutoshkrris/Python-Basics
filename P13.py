ea64b947ef175f4e469d5bf74f0938797fb1e682
Ashutosh Krishna <47353498+ashutoshkrris@users.noreply.github.com>
2020-02-08 19:44:26 +0530

    Check for prime number

---
+++ P13.py
@@ -0,0 +1,19 @@
+num = int(input("Enter the number : "))
+if(num>1):
+    for i in range(2,num):
+        if(num%2==0):
+            print(num,"is not a prime number.")
+            break
+    else:
+        print(num,"is a prime number.")
+else:
+    print("Enter number greater than",num)
+
+
+'''                                     Output
+                                Enter the number : 1
+                                Enter number greater than  1
+                                        OR
+                                Enter the number : 13
+                                13 is a prime number.        
+'''
