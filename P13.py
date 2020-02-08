be447c46120837c9cf5f223c0efc2b79690b84df
Ashutosh Krishna <47353498+ashutoshkrris@users.noreply.github.com>
2020-02-08 19:49:00 +0530

    Update P13.py

--- P13.py
+++ P13.py
@@ -1,7 +1,7 @@
 num = int(input("Enter the number : "))
 if(num>1):
     for i in range(2,num):
-        if(num%2==0):
+        if(num%i==0):
             print(num,"is not a prime number.")
             break
     else:
