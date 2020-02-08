1cf9e1119c2790a68d8ab255b53d52cc43079c55
Ashutosh Krishna <47353498+ashutoshkrris@users.noreply.github.com>
2020-02-08 15:08:21 +0530

    Generate a random number
    We can generate random integers between a given range using random module.

---
+++ P7.py
@@ -0,0 +1,13 @@
+import random
+
+x = int(input("Enter the lower limit of the range : "))
+y = int(input("Enter the upper limit of the range : "))
+random_number = random.randint(x,y)
+print(random_number)
+
+
+'''                                         Output
+                            Enter the lower limit of the range : 0
+                            Enter the upper limit of the range : 9
+                            4
+'''
