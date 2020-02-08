432f47014b17ed9425a1b76947e17f64fe434cbd
Ashutosh Krishna <47353498+ashutoshkrris@users.noreply.github.com>
2020-02-08 20:12:13 +0530

    Table of a number

---
+++ P16.py
@@ -0,0 +1,21 @@
+num = int(input("Enter the number : "))
+if(num>1):
+    for i in range(1,11):
+        mul = num*i
+        print(num,"X",i,"=",mul)
+
+
+
+'''                                     Output
+                                Enter the number : 4
+                                4 X 1 = 4
+                                4 X 2 = 8
+                                4 X 3 = 12
+                                4 X 4 = 16
+                                4 X 5 = 20
+                                4 X 6 = 24
+                                4 X 7 = 28
+                                4 X 8 = 32
+                                4 X 9 = 36
+                                4 X 10 = 40
+'''
