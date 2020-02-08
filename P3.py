9009bf927f2bd87272f4c3bdfc5bd638ffe0cc66
Ashutosh Krishna <47353498+ashutoshkrris@users.noreply.github.com>
2020-02-08 14:32:52 +0530

    Square root of a number

---
+++ P3.py
@@ -0,0 +1,7 @@
+num = int(input("Enter the number : "))
+print("Square root of "+str(num)+" is "+str(num**0.5))
+
+'''                 Output
+            Enter the number : 16
+            Square root of 16 is 4.0
+'''
