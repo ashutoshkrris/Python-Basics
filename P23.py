101dec22c78a239208bc707535373aa0fd6359f4
Ashutosh Krishna <47353498+ashutoshkrris@users.noreply.github.com>
2020-02-11 21:27:37 +0530

    ASCII value of any character

---
+++ P23.py
@@ -0,0 +1,9 @@
+ch = input("Enter a character : ")
+print("ASCII value of",ch,"is",ord(ch))
+
+
+
+'''                             Output
+                        Enter a character : A
+                        ASCII value of A is 65
+'''
