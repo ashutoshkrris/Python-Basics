6594bb81a4d689a6f463c4c3fd1dfb45173bde14
Ashutosh Krishna <47353498+ashutoshkrris@users.noreply.github.com>
2020-02-11 21:21:21 +0530

    Decimal to Binary , Octal and Hexadecimal form

---
+++ P22.py
@@ -0,0 +1,13 @@
+dec = int(input("Enter the number in decimal form : "))
+print(dec,"in Binary format is",bin(dec))
+print(dec,"in Octal format is",oct(dec))
+print(dec,"in Hexadecimal format is",hex(dec))
+
+
+
+'''                                     Output
+                        Enter the number in decimal form : 15
+                        15 in Binary format is 0b1111
+                        15 in Octal format is 0o17
+                        15 in Hexadecimal format is 0xf
+'''
