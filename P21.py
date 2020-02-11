c8688962ac3e21dd32f80f2980bb0bcd5020aec6
Ashutosh Krishna <47353498+ashutoshkrris@users.noreply.github.com>
2020-02-11 21:17:29 +0530

    Update P21.py

--- P21.py
+++ P21.py
@@ -6,3 +6,16 @@ for i in range(n):
 num = int(input("Enter the number for which divisibility is to be checked : "))
 result = list(filter(lambda x: (x%num==0),my_list))
 print("Numbers divisible by",num,"are",result)
+
+
+
+'''                                     Output
+                    How may numbers you want to put in the list? : 5
+                    Enter the element of list : 11
+                    Enter the element of list : 12
+                    Enter the element of list : 13
+                    Enter the element of list : 14
+                    Enter the element of list : 15
+                    Enter the number for which divisibility is to be checked : 2
+                    Numbers divisible by 2 are [12, 14]
+'''
